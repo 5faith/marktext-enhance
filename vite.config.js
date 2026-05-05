8d4b69b933a6eee7920ff6f574f0059a93673b9e
5faith <1475307570@qq.com>
2026-05-05 21:35:17 +0800

    feat(phase6): remove @vue/compat and fix compat warnings

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,6 @@ export default defineConfig(({ command }) => {
         'common': resolve(__dirname, 'src/common'),
         'muya': resolve(__dirname, 'src/muya'),
         'snapsvg': resolve(__dirname, 'src/muya/lib/assets/libs/snap.svg-min.js'),
-        'vue': '@vue/compat',
         'eve': resolve(__dirname, 'node_modules/eve-raphael'),
         'fs-extra': resolve(__dirname, 'src/renderer/shims/fs-extra.js'),
         'fs/promises': resolve(__dirname, 'src/renderer/shims/fs-promises.js'),
@@ -36,7 +35,7 @@ export default defineConfig(({ command }) => {
         'electron': resolve(__dirname, 'src/renderer/shims/electron.js')
       },
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-      dedupe: ['vue', '@vue/compat']
+      dedupe: ['vue']
     },
     define: {
       __static: JSON.stringify(resolve(__dirname, 'static')),
@@ -46,14 +45,6 @@ export default defineConfig(({ command }) => {
       vue({
         template: {
           compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-              GLOBAL_MOUNT: false,
-              GLOBAL_EXTEND: false,
-              GLOBAL_VUE: false,
-              OPTIONS_DATA_FN: false,
-              COMPILER_V_FOR_OBJECT_ORDER: false
-            }
           }
         }
       }),
